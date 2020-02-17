b63c1d2bdc532fdbb348d8a82c12956c913ff47e
hlin <Zxsaqw12@&$>
2020-02-17 18:05:05 -0500

    not done

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -6,11 +6,7 @@ public class Jumper {
      * Complete the function below.
      */
     public int jumps(int flagHeight, int jumpHeight) {
-        int num = 0;
-        for (int i = 0; i < flagHeight; i+=jumpHeight) {
-            num++;
-        }
-        return num;
-        //only got one test to pass lol...
+
+        return (flagHeight/jumpHeight) + (flagHeight % jumpHeight);
     }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -3,6 +3,20 @@ package com.zipcodewilmington.assessment1.part5;
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-        return null;
+
+        int num = 0;
+        String reverse = "";
+        for (int i = input.length() - 1; i >= 0 ; i--) {
+            reverse += input.charAt(i);
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j < input.length(); j++) {
+                if (input.substring(i, j).equals(reverse.substring(i, j))){
+                    num++;
+                }
+            }
+        }
+        return num;
     }
 }
